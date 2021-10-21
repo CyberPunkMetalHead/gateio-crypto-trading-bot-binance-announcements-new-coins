0e3dd1fdb78fa71199c7a7095781242bc86c6d57
TraSo-FSE <90628394+TraSo-FSE@users.noreply.github.com>
2021-10-21 16:27:49 +0200

    Update store_order.py

--- store_order.py
+++ store_order.py
@@ -1,16 +1,20 @@
 import json
+import os
 
 
 def store_order(file, order):
-        """
-        Save order into local json file
-        """
-        with open(file, 'w') as f:
-            json.dump(order, f, indent=4)
+    """
+    Save order into local json file
+    """
+    with open(file, 'w') as f:
+        json.dump(order, f, indent=4)
+
 
 def load_order(file):
     """
     Update Json file
     """
+    if not os.path.isfile(file) or os.stat(file).st_size == 0:
+        return {}
     with open(file, "r+") as f:
         return json.load(f)
