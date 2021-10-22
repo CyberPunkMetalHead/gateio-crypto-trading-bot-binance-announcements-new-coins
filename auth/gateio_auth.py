48806354c56ba684b9f102af74aa6bd696ac0d9d
Blake <blakerphill99@gmail.com>
2021-10-22 03:00:08 -0400

    added some qol changes to config file loading

--- auth/gateio_auth.py
+++ auth/gateio_auth.py
@@ -1,9 +1,15 @@
 import yaml
 from gate_api import ApiClient, Configuration, Order, SpotApi
-
+from load_config import *
 
 def load_gateio_creds(file):
-    with open(file) as file:
-        auth = yaml.load(file, Loader=yaml.FullLoader)
+    try:
+        with open(file) as file:
+            auth = yaml.load(file, Loader=yaml.FullLoader)
+    except FileNotFoundError as e:
+        # if auth config doesn't exist, query user if they want help
+        auth = create_config(file)
+        raise FileNotFoundError("check that your "+file+" exists, use "+file.split(".")[0]+
+                                ".example.yml as a template. e: " + str(e))
 
     return Configuration(key=auth['gateio_api'], secret=auth['gateio_secret'])
