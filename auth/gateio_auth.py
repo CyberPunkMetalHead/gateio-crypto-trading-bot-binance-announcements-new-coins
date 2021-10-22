dd80121c1694e1f36bc4fcf77bc33190d6553e3d
Blake <blakerphill99@gmail.com>
2021-10-22 03:07:46 -0400

    move return to more readable spot

--- auth/gateio_auth.py
+++ auth/gateio_auth.py
@@ -6,10 +6,9 @@ def load_gateio_creds(file):
     try:
         with open(file) as file:
             auth = yaml.load(file, Loader=yaml.FullLoader)
+            return Configuration(key=auth['gateio_api'], secret=auth['gateio_secret'])
     except FileNotFoundError as e:
         # if auth config doesn't exist, query user if they want help
         create_config(file)
         raise FileNotFoundError("check that your "+file+" exists, use "+file.split(".")[0]+
                                 ".example.yml as a template. e: " + str(e))
-
-    return Configuration(key=auth['gateio_api'], secret=auth['gateio_secret'])
