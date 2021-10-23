8c13ef2fb47c9058d889dd483fe854a075e1ba63
Jake Radcliff <jake.radcliff@verizon.net>
2021-10-22 20:53:53 -0500

    Add default logging settings

--- logger.py
+++ logger.py
@@ -7,6 +7,10 @@ config = load_config('config.yml')
 
 logger = logging
 
+# Set default log settings
+log_level = 'INFO'
+log_file = 'bot.log'
+
 # Get logging variables
 log_level = config['LOGGING']['LOG_LEVEL']
 log_file = config['LOGGING']['LOG_FILE']
