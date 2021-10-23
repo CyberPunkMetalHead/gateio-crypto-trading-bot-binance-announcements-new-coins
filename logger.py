939bf49d2b3dbe503c8f0c3e38bfc112faf6c3dd
Jake Radcliff <jake.radcliff@verizon.net>
2021-10-22 23:09:23 -0500

    Fix log level not changing

--- logger.py
+++ logger.py
@@ -5,7 +5,7 @@ from load_config import *
 # loads local configuration
 config = load_config('config.yml')
 
-logger = logging
+log = logging
 
 # Set default log settings
 log_level = 'INFO'
@@ -14,6 +14,8 @@ log_file = 'bot.log'
 # Get logging variables
 log_level = config['LOGGING']['LOG_LEVEL']
 log_file = config['LOGGING']['LOG_FILE']
-logger.basicConfig(level=log_level, format='%(asctime)s %(levelname)s: %(message)s',
+log.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
                    handlers=[logging.FileHandler(log_file), logging.StreamHandler()])
-
+logger = logging.getLogger(__name__)
+level = logging.getLevelName(log_level)
+logger.setLevel(level)
