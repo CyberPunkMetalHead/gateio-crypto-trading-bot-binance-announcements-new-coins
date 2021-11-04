99ab2817115049ec439333cfc624806e99e99da2
Anthony Carroll <busyuqboy@yahoo.com>
2021-11-04 14:31:15 -0400

    Merge change from upstream

--- main.py
+++ main.py
@@ -377,7 +377,8 @@ def main():
                         
                 else:
                     logger.warning(f'{announcement_coin=} is not supported on gate io')
-                    os.remove("new_listing.json")
+                    if os.path.isfile('new_listing.json'):
+                        os.remove("new_listing.json")
                     logger.debug('Removed new_listing.json due to coin not being '
                                   'listed on gate io')
             else:
