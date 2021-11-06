67c8feb22bff9ea124a7314e3e9daf3c367cdb3e
Jake Radcliff <jake.radcliff@verizon.net>
2021-11-06 12:38:03 -0500

    Update variable name for clarity

--- new_listings_scraper.py
+++ new_listings_scraper.py
@@ -25,11 +25,11 @@ def get_last_coin():
     """
     logger.debug("Pulling announcement page")
     # Generate random query/params to help prevent caching
-    rand_page = random.randint(1, 200)
+    rand_page_size = random.randint(1, 200)
     letters = string.ascii_letters
     random_string = ''.join(random.choice(letters) for i in range(random.randint(10, 20)))
     random_number = random.randint(1, 99999999999999999999)
-    queries = ["catalogId=48", "pageNo=1", f"pageSize={str(rand_page)}", f"rnd={str(time.time())}", f"{random_string}={str(random_number)}"]
+    queries = ["catalogId=48", "pageNo=1", f"pageSize={str(rand_page_size)}", f"rnd={str(time.time())}", f"{random_string}={str(random_number)}"]
     random.shuffle(queries)
     logger.debug(f"Queries: {queries}")
     request_url = f"https://www.binance.com/bapi/composite/v1/public/cms/article/catalog/list/query?{queries[0]}&{queries[1]}&{queries[2]}&{queries[3]}&{queries[4]}"
