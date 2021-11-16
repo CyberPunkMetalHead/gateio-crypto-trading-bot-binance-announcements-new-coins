17b92fba091fb739d30d32feef07a4a9b3409374
Daniel Pérez García <daniel.perez@shargo.io>
2021-11-16 10:15:22 +0100

    feat: add python alpine dockerfile and restructure folders

--- tests/test_new_listings_scraper.py
+++ tests/test_new_listings_scraper.py
@@ -3,7 +3,7 @@ import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
-from new_listings_scraper import get_announcement
+from src.new_listings_scraper import get_announcement
 
 
 def test_latest_announcement():
