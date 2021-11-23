783f2fe2edfc3161851ebae2dc0f42aa9ff90bfa
Lukas Kiefer <kiefermaniac@gmail.com>
2021-11-23 08:34:08 +0100

    incorporate feedback

--- send_telegram.py
+++ send_telegram.py
@@ -5,36 +5,24 @@ from load_config import *
 
 config = load_config('config.yml')
 
-send_success = False
+telegram_enabled = config['TELEGRAM']['ENABLED']
 
 if "BOT_TOKEN" in config['TELEGRAM'] and "BOT_CHAT_ID" in config['TELEGRAM']:
    bot_token = config['TELEGRAM']['BOT_TOKEN']
    bot_chatID = str(config['TELEGRAM']['BOT_CHAT_ID'])
-   send_success = True
 else:
+   # bail when config is not supplied
    logger.info('Telegram not configured -> disabled')
+   telegram_enabled = False
 
 def send_telegram(message):
 
-   global send_success
+   global telegram_enabled
 
-   if send_success:
+   if not telegram_enabled:
+      pass
 
-      for var in [bot_token, bot_chatID]:
-         if var is None:
-            return
-
-      send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message
-      logger.info('TELEGRAM sending '+message)
-      
-      response = requests.get(send_text)
-      logger.debug('TELEGRAM '+str(response.json()))
-
-      if response.status_code != 200:
-         logger.info('failed to send telegram message, disabling')
-         send_success = False
-      else:
-         send_success = True
-
-   else:
-      logger.debug('Not sending to telegram, last send failed')
+   send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message   
+   response = requests.get(send_text)
+   if response.status_code != 200:
+      logger.error(f'failed to send telegram message: {response}')
