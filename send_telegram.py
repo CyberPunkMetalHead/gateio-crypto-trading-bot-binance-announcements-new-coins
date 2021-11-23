d5b4e2cbe963e32937c1bcb7649acbf52bba5f19
Lukas Kiefer <kiefermaniac@gmail.com>
2021-11-23 08:45:11 +0100

    cleanup

--- send_telegram.py
+++ send_telegram.py
@@ -5,22 +5,25 @@ from load_config import *
 
 config = load_config('config.yml')
 
-telegram_enabled = config['TELEGRAM']['ENABLED']
+telegram_enabled = "ENABLED" in config['TELEGRAM'] and config['TELEGRAM']['ENABLED']
 
-if "BOT_TOKEN" in config['TELEGRAM'] and "BOT_CHAT_ID" in config['TELEGRAM']:
-   bot_token = config['TELEGRAM']['BOT_TOKEN']
-   bot_chatID = str(config['TELEGRAM']['BOT_CHAT_ID'])
+if not telegram_enabled:
+   logger.info('Telegram is disabled')
 else:
-   # bail when config is not supplied
-   logger.info('Telegram not configured -> disabled')
-   telegram_enabled = False
+   if "BOT_TOKEN" in config['TELEGRAM'] and "BOT_CHAT_ID" in config['TELEGRAM']:
+      # read config
+      bot_token = config['TELEGRAM']['BOT_TOKEN']
+      bot_chatID = str(config['TELEGRAM']['BOT_CHAT_ID'])
+      logger.info('Telegram initialized')
+   else:
+      # bail when config is not supplied
+      logger.info('Telegram not configured -> disabled')
+      telegram_enabled = False
 
 def send_telegram(message):
 
-   global telegram_enabled
-
    if not telegram_enabled:
-      pass
+      return
 
    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message   
    response = requests.get(send_text)
